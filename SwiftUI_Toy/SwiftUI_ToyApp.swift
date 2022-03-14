a9f41825777a9004b9be25e68fe7baf40c3ed44d
AlexHwang <cksgh0316@gmail.com>
2022-03-14 19:09:04 +0900

    Initial Commit

---
+++ SwiftUI_Toy/SwiftUI_ToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToyApp.swift
+//  SwiftUI_Toy
+//
+//  Created by AlexHwang on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
